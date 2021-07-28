cefefc73e636807eeed8d85de591eef2d9e6cdc7
Franklyn Reyes <franklynreyes777@yahoo.com>
2021-07-28 15:13:12 -0400

    Update Landing.js
    updated url

--- src/Homepage/Landing.js
+++ src/Homepage/Landing.js
@@ -12,7 +12,7 @@ const Landing = () => {
   }, []);
 
   const getUser = () => {
-    fetch("http://localhost:8080/users/login", {
+    fetch("https://capstone-movie.herokuapp.com/users/login", {
       method: "GET",
       credentials: "include",
     })
